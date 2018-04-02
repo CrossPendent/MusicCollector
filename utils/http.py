105b37bc524b2e12218ba8924cfe8eff843f7323
CrossPendent <yaho818@gmail.com>
2018-04-02 21:33:01 +0900

    reduce http agent and change the http's header

--- utils/http.py
+++ utils/http.py
@@ -7,23 +7,19 @@ from utils import debug
 
 
 def getHTMLDocument(url, autoRetry=True):
-  listAgent = ['Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
-               'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-               'Mozilla/5.0 (Windows NT 5.1; rv:6.0.2) Gecko/20100101 Firefox/6.0.2',
-               'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
-               ]
+  listAgent = ['Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36']
   retryDelay = [0.1, 0.5, 1, 2, 5, 10, 30, 60, 180, 300]
   conLoop = True
   agentCount = 0
   delayCount = 0
   while(conLoop):
     opener = urllib.request.build_opener()
-    opener.addheaders = [('Accept', 'text/html, application/xhtml+xml, */*'),
-                         ('Accept-Language', 'ko-KR'),
-                         ('User_agent', listAgent[agentCount]),
-                         ('Host', 'www.melon.com'),
-                         ('DNT', '1'),
+    opener.addheaders = [('Host', 'www.melon.com'),
                          ('Connection', 'Keep-Alive'),
+                         ('Upgrade-Insecure-Requests', '1'),
+                         ('User_agent', listAgent[agentCount]),
+                         ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'),
+                         ('Accept-Language', 'en-US,en;q=0.9,ko-KR;q=0.8,ko;q=0.7'),
                          ("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")]
     try:
       html = opener.open(url)
