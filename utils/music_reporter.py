07cf87e14c05346a6f1d98fd75d015eb74702015
CrossPendent <yaho818@gmail.com>
2018-04-03 18:55:57 +0900

    add file counter in music reporter

--- utils/music_reporter.py
+++ utils/music_reporter.py
@@ -8,14 +8,26 @@ class MusicReporter():
     if not os.path.exists(output_dir):
       os.mkdir(output_dir)
     file_path = os.path.join(output_dir, filename)
+    self._file_count_ = 0
     self._report_file_ = open(file_path, mode='a+', encoding='utf-8')
 
   def __del__(self):
+    if self._file_count_ > 0:
+      self.writeEndFileLine()
     if not self._report_file_ == None:
       self._report_file_.close()
 
+  def writeEndFileLine(self):
+    postfix = ' was'
+    if self._file_count_ > 1:
+      postfix = 's were'
+    self._report_file_.write('<<<<< The {} file{} updated. >>>>>\r\n'.format(self._file_count_, postfix))
+    self._report_file_.flush()
+
   def updateMusic(self, title, link):
-    self._report_file_.write('[{}] {} {}\r\n'.format(dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), title, link))
+    self._file_count_ += 1
+    self._report_file_.write('[{}] #{} {} {}\r\n'.format(dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                                         self._file_count_, title, link))
     self._report_file_.flush()
 
 def generateReportFromDebugLog(target_dir, srclog_filename, report_filename):
@@ -24,12 +36,17 @@ def generateReportFromDebugLog(target_dir, srclog_filename, report_filename):
   mr = MusicReporter(target_dir, report_filename)
 
   for rdBuf in src_file:
+    if rdBuf.find('***** End *****') >= 0:
+        if mr != None:
+          del mr
+        mr = MusicReporter(target_dir, report_filename)
     if rdBuf.find('www.youtube.co.kr/watch') >= 0:
       parsed = rdBuf.split('\'')
       title, link = parsed[1], parsed[3]
       mr.updateMusic(title, link)
 
-  del mr
+  if mr != None:
+    del mr
   src_file.close()
 
 if __name__ == '__main__':
