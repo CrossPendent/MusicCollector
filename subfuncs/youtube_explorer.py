505a0a30253bb7a35f830c13f4760c1e486054e1
CrossPendent <yaho818@gmail.com>
2018-05-29 10:17:42 +0900

    change the flow to check the existing file

--- subfuncs/youtube_explorer.py
+++ subfuncs/youtube_explorer.py
@@ -180,15 +180,7 @@ def getSongFromYouTube(artist, title, songID, lyric, albumID, baseMusicDir, base
                        isOverwriteMode=False, music_reporter=None):
   audio_name = '{}-{}'.format(artist, title)
   query = '{} audio'.format(audio_name)
-  debug.log('Looking for youtube by the query \'{}\'...'.format(query))
-  list = find_youtube(query)
-  retry = 0
-  while len(list) <= 1 and retry < 5:
-    debug.log('Youtube list couldn\'t be gotten. retry...')
-    list = find_youtube(query)
-    retry += 1
-  debug.log('trying to download \'' + query + '\'...')
-
+  # check whether mp3 file already exists.
   if not os.path.exists(baseMusicDir):
     os.mkdir(baseMusicDir)
   filename = convertQueryToFilename(audio_name)
@@ -198,9 +190,9 @@ def getSongFromYouTube(artist, title, songID, lyric, albumID, baseMusicDir, base
   mp3_dir = os.path.join(convertQueryToFilename(artist), albumID)
   if not os.path.exists(os.path.join(baseMusicDir, mp3_dir)):
     os.mkdir(os.path.join(baseMusicDir, mp3_dir))
-  mp3_filename = filename+'.mp3'
+  mp3_filename = filename + '.mp3'
   mp3_path = os.path.join(mp3_dir, mp3_filename)
-  # check whether mp3 file already exists.
+
   isSkip = False
   if os.path.exists(os.path.join(baseMusicDir, mp3_path)):
     if isOverwriteMode:
@@ -210,6 +202,15 @@ def getSongFromYouTube(artist, title, songID, lyric, albumID, baseMusicDir, base
       debug.log('{} is already exist. Downloading will be skipped.'.format(mp3_path))
       isSkip = True
   if not isSkip:
+    debug.log('Looking for youtube by the query \'{}\'...'.format(query))
+    list = find_youtube(query)
+    retry = 0
+    while len(list) <= 1 and retry < 5:
+      debug.log('Youtube list couldn\'t be gotten. retry...')
+      list = find_youtube(query)
+      retry += 1
+    debug.log('trying to download \'' + query + '\'...')
+
     file_name = download_audio_from_youtube(list[0], baseMusicDir, audio_name, music_reporter)
     debug.log('\'' + file_name + '\' was downloaded.')
     debug.log('\'' + file_name + '\' is converting...')
